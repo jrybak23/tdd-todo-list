61c6e20b3ea515a45e0f64177df1c3d4e1bb8e4c
Ihor Rybak <jrybak23@gmail.com>
2021-05-14 15:37:03 +0300

    add tmpfs volume

--- src/test/java/com/example/config/PostgresResource.java
+++ src/test/java/com/example/config/PostgresResource.java
@@ -9,10 +9,14 @@ import static java.util.Collections.singletonMap;
 
 public class PostgresResource implements QuarkusTestResourceLifecycleManager {
 
+    private static final String POSTGRES_CONTAINER_DATA_PATH = "/var/lib/postgresql/data";
+    private static final String TMPFS_OPTIONS = ""; // https://docs.docker.com/storage/tmpfs/#specify-tmpfs-options
+
     private final static PostgreSQLContainer<?> db = new PostgreSQLContainer<>("postgres:13")
             .withDatabaseName("testdb")
             .withUsername("postgres")
-            .withPassword("postgres");
+            .withPassword("postgres")
+            .withTmpFs(Map.of(POSTGRES_CONTAINER_DATA_PATH, TMPFS_OPTIONS));
 
     @Override
     public Map<String, String> start() {
