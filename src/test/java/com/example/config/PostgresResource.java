b6a01f35d111a62becb81b92e79955765e814bdb
Ihor Rybak <jrybak23@gmail.com>
2022-01-23 22:17:28 +0200

    refactor. tmpfs options

--- src/test/java/com/example/config/PostgresResource.java
+++ src/test/java/com/example/config/PostgresResource.java
@@ -10,13 +10,13 @@ import static java.util.Collections.singletonMap;
 public class PostgresResource implements QuarkusTestResourceLifecycleManager {
 
     private static final String POSTGRES_CONTAINER_DATA_PATH = "/var/lib/postgresql/data";
-    private static final String TMPFS_OPTIONS = ""; // https://docs.docker.com/storage/tmpfs/#specify-tmpfs-options
+    private static final String TMPFS_OPTIONS = "rw"; // https://docs.docker.com/storage/tmpfs/#specify-tmpfs-options
 
     private final static PostgreSQLContainer<?> db = new PostgreSQLContainer<>("postgres:13")
             .withDatabaseName("testdb")
             .withUsername("postgres")
             .withPassword("postgres")
-            .withTmpFs(Map.of(POSTGRES_CONTAINER_DATA_PATH, TMPFS_OPTIONS));
+            .withTmpFs(Map.of(POSTGRES_CONTAINER_DATA_PATH, TMPFS_OPTIONS)); // Persist data to RAM instead of disk
 
     @Override
     public Map<String, String> start() {
