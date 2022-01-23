36dcdeb37cfa8c68c9ff20b559c76fed0e45b558
Ihor Rybak <jrybak23@gmail.com>
2022-01-23 21:41:58 +0200

    disable native test

--- src/test/java/com/example/NativeExampleResourceIT.java
+++ src/test/java/com/example/NativeExampleResourceIT.java
@@ -2,8 +2,10 @@ package com.example;
 
 import com.example.controllers.ExampleResourceTest;
 import io.quarkus.test.junit.NativeImageTest;
+import org.junit.jupiter.api.Disabled;
 
 @NativeImageTest
+@Disabled
 public class NativeExampleResourceIT extends ExampleResourceTest {
 
     // Execute the same tests but in native mode.
